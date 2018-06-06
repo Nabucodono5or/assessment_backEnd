719b0a2f8e8dbae23433a74ed4a03a34543c5c47
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-06-06 00:30:05 -0300

    usado ui-router entre modulos com sucesso

--- app/components/noticias/noticias.js
+++ app/components/noticias/noticias.js
@@ -1,3 +1,28 @@
 (function(){
-  angular.module('noticias',[]);
+  angular.module('noticias',['ui.router']);
+})();
+
+(function(){
+
+  angular.module('noticias').config(function($stateProvider, $urlRouterProvider) {
+    
+  $urlRouterProvider.otherwise('/noticias');
+
+    $stateProvider.state('noticias', {
+      url: '/noticias',
+      component: 'noticias'
+    });
+
+  });
+
+  /*
+
+    $urlRouterProvider.otherwise('/noticias')
+
+    .state('noticias', {
+      url: '/noticias',
+      component: 'noticias'
+    })
+  
+  */
 })();
