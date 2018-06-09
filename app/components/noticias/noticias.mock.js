0769b301ed450ee2ebe53af516d7d4f30e1acc10
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-06-08 21:59:32 -0300

    criado mocks de post e delete

--- app/components/noticias/noticias.mock.js
+++ app/components/noticias/noticias.mock.js
@@ -5,6 +5,10 @@
   function noticiasRun($httpBackend, noticiasMockService) {
     baseUrl = '/api/noticias';
     $httpBackend.whenGET(baseUrl).respond(noticiasMockService.lista);
+  
+    $httpBackend.whenPOST('/api/noticias').respond(204, '');
+
+    $httpBackend.whenDELETE('/api/noticias'+'\d+').respond(204, '')
   }
 
 })();
