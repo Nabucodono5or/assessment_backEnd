7919547b15950015742887bb4c6634715d63ebab
Jefferson Abreu <pereirajefferson06@gmail.com>
2018-06-08 23:09:37 -0300

    funções dos botões respodendo, faltando apenas resolver as requisiçoes PUT e DELETE

--- app/components/noticias/noticias.component.js
+++ app/components/noticias/noticias.component.js
@@ -21,13 +21,36 @@
 
     this.onButtonClicked = () => {
       if(this.editando){
-        noticiasService.update(this.noticia);
+        noticiasService.update(this.noticia).then((response) => {
+          this.carregarDados();
+          console.log('sucesso');
+        }, (err) => {
+          console.log(err);
+        });
       }else{
-        noticiasService.add(this.noticia);
+        noticiasService.add(this.noticia).then((response) => {
+          this.carregarDados();
+          console.log('sucesso');
+        }, (err) => {
+          console.log(err);
+        });
       }
 
       delete this.noticia;
       this.editando = false;
+    }
+
+
+    this.onDeleteButtonClicked = () => {
+      if(this.noticia){
+        noticiasService.remove(this.noticia).then((response) => {
+          this.carregarDados();
+          console.log('sucesso');
+        }, (err) => {
+          console.log(err);
+        });
+        delete this.noticia;
+      }
     }
 
   }
@@ -39,7 +62,17 @@
   angular.module('noticias').component('noticiascomp',{
     template: '<div><div ng-repeat="noticia in $ctrl.lista" ng-click="$ctrl.clicouNoticia(noticia)">'+'<p ng-bind="noticia.titulo"></p>'+
     '<p ><span ng-bind="noticia.autor"></span>, <span ng-bind="noticia.data"></span></p>'
-    +'<img src="noticia.imagem" alt="imagem">'+'<p ng-bind="noticia.mensagem"></p>'+'</div>'+'</div>',
+    +'<img src="noticia.imagem" alt="imagem">'+'<p ng-bind="noticia.mensagem"></p>'+'</div>'+
+    '<label for="">Titulo <input type="text" ng-model="$ctrl.noticia.titulo"> </label> <br>'
+    +'<label for=""> Mensagem<textarea name="msg"  cols="30" rows="10" ng-model="$ctrl.noticia.mensagem"></textarea><br> </label>'
+    +'<label for="">Autor<input type="text" ng-model="$ctrl.noticia.autor"></label>'
+    +'<button ng-click="$ctrl.onButtonClicked()" >'
+    +'{{$ctrl.editando ? "Atualizar" : "Adicionar"}}'
+    +'</button>'
+    +'<button ng-click="$ctrl.onDeleteButtonClicked()">'
+    +'{{$ctrl.editando ? "Deletar" : "Deletar Tudo"}}'
+    +'</button> '
+    +'</div>',
     controller: noticiasController
   });
 })();
