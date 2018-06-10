7898d3207646cc6fb700c9b5ef801c96bbf93854
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-06-10 15:36:55 -0300

    criado os arquivos base do componente navbar

--- app/common/common.js
+++ app/common/common.js
@@ -1,3 +1,3 @@
 (function () {
-  angular.module('common', ['form', 'lista']);
+  angular.module('common', ['form', 'lista', 'navbar']);
 })();
---
+++ app/common/navbar/navbar.component.js
@@ -0,0 +1,5 @@
+(function () {
+  angular.module('navbar').component('navbarcomp',{
+    template: ``
+  });
+})();
