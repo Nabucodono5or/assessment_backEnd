f6b5ec0818c09bc6b6ae3533785579518fb0c60c
Jefferson Abreu <nabucodonosor310@gmail.com>
2018-06-14 16:13:59 -0300

    modificação da mockagem e efeito

--- app/components/noticias/noticias.mock.js
+++ app/components/noticias/noticias.mock.js
@@ -1,17 +1,23 @@
 (function () {
-  
+
   angular.module('noticias').run(noticiasRun);
 
   function noticiasRun($httpBackend, noticiasMockService) {
     baseUrl = '/api/noticias';
     $httpBackend.whenGET(baseUrl).respond(noticiasMockService.lista);
-  
-    $httpBackend.whenPOST('/api/noticias').respond(204, '');
+
+    $httpBackend.whenPOST('/api/noticias').respond(function(method, url, data) {
+         var newNoticia = angular.fromJson(data);
+         console.log(data);
+         noticiasMockService.lista.push(newNoticia);
+
+        return [200, newNoticia, {}];
+    });
 
     $httpBackend.whenDELETE('/api/noticias').respond(function(method, url, data) {
       return [200, '', {}];
     });
-    
+
     $httpBackend.whenPUT(/\/noticias\/(\d+)/).respond(204, '');
   }
 
